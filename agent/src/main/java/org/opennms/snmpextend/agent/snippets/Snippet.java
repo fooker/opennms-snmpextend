96f59ae3dcdad6555b3b1338bdbb9aa5d1055315
Dustin Frisch <fooker@lab.sh>
2015-06-24 20:42:34 +0200

    Support compiling for script engines that support it

--- agent/src/main/java/org/opennms/snmpextend/agent/snippets/Snippet.java
+++ agent/src/main/java/org/opennms/snmpextend/agent/snippets/Snippet.java
@@ -5,9 +5,7 @@ import org.opennms.snmpextend.agent.args.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.script.Bindings;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
+import javax.script.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -26,8 +24,14 @@ public class Snippet {
 
     private final ScriptEngine scriptEngine;
 
-    private Instant cachedTime = Instant.MIN;
-    private Result cachedData = null;
+    private Instant cachedTime;
+    private Result cachedData;
+
+    /**
+     * The cache for the script compiled for faster execution (can be {@code null} if the engine does not support
+     * compiling).
+     */
+    private CompiledScript script;
 
     public Snippet(final Config config, final Path path) {
         this.config = config;
@@ -47,6 +51,9 @@ public class Snippet {
         this.scriptEngine = SnippetManager.SCRIPT_ENGINE_MANAGER.getEngineByExtension(suffix);
 
         LOG.trace("Use script engine for {} : {} > {}", filename, suffix, this.scriptEngine);
+
+        // Flush to start with an empty but well defined cache
+        this.flush();
     }
 
     public Path getPath() {
@@ -70,11 +77,22 @@ public class Snippet {
             bindings.put("prefix", this.prefix);
             bindings.put("results", result);
 
-            try (final BufferedReader r = Files.newBufferedReader(this.path)) {
-                this.scriptEngine.eval(r, bindings);
+            if (this.script != null) {
+                // Execute the compiled script
+                try {
+                    this.script.eval(bindings);
+                } catch (final ScriptException e) {
+                    throw Throwables.propagate(e);
+                }
 
-            } catch (final IOException | ScriptException e) {
-                throw Throwables.propagate(e);
+            } else {
+                // Execute the script directly from source
+                try (final BufferedReader r = Files.newBufferedReader(this.path)) {
+                    this.scriptEngine.eval(r, bindings);
+
+                } catch (final IOException | ScriptException e) {
+                    throw Throwables.propagate(e);
+                }
             }
 
             this.cachedTime = now;
@@ -85,7 +103,26 @@ public class Snippet {
     }
 
     public void flush() {
+        // Reset the cache data and time
         this.cachedTime = Instant.MIN;
         this.cachedData = null;
+
+        // Try to compile the script if the engine supports it
+        if (this.scriptEngine instanceof Compilable) {
+            final Compilable compilable = (Compilable) this.scriptEngine;
+
+            try (final BufferedReader r = Files.newBufferedReader(this.path)) {
+                // Compile the script and cache it
+                this.script = compilable.compile(r);
+
+                LOG.trace("Compiled script successfully: {}", this.path);
+
+            } catch (final IOException | ScriptException e) {
+                LOG.error("Failed to compile the script: {}", this.path, e);
+
+                // Don't propagate the exception to act like the engine does not support compiling
+                this.script = null;
+            }
+        }
     }
 }
